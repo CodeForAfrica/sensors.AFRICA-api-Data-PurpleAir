7195ee38a813fe24eee8671ef91f75c2e221448a
esir <esirkings@gmail.com>
2021-01-19 11:28:24 +0300

    Add sentry for errors monitoring.

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import sentry_sdk
+
 from chalice import Chalice, Rate
 from chalicelib import service
+from chalicelib.settings import SCHEDULE_RATE, SENTRY_DSN
 
+from sentry_sdk.integrations.chalice import ChaliceIntegration
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[ChaliceIntegration()],
+    traces_sample_rate=0.5
+)
 
 app = Chalice(app_name="sensors-africa-purpleair")
 
-@app.schedule(Rate(10, unit=Rate.MINUTES))
+@app.schedule(Rate(int(SCHEDULE_RATE), unit=Rate.MINUTES))
 def scheduled(event):
     return service.run()
